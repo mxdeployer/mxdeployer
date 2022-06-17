a83ba4f567a1d77f314aa37fce7079c671d931fc
Andy Sherwood <xinmyname@gmail.com>
2022-06-17 11:33:22 -0700

    Just log a ticker for now until I figure out how the best way to receive msgs

--- cmd/mxdeployservice/main.go
+++ cmd/mxdeployservice/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/mxdeployer/mxdeployer/internal/logfactory"
+	"github.com/mxdeployer/mxdeployer/internal/service"
 )
 
 func main() {
@@ -15,5 +17,35 @@ func main() {
 
 	log.SetOutput(logwriter)
 
-	log.Println("Shutting down!")
+	service.Run(&mxdeployservice{})
+}
+
+type mxdeployservice struct {
+	ticker *time.Ticker
+}
+
+func (s *mxdeployservice) Name() string {
+	return "mxdeployservice"
+}
+
+func (s *mxdeployservice) Start() {
+
+	log.Println("Starting...")
+
+	s.ticker = time.NewTicker(1 * time.Second)
+	go s.tick()
+}
+
+func (s *mxdeployservice) Stop() {
+
+	log.Println("Stopping...")
+
+	s.ticker.Stop()
+}
+
+func (s *mxdeployservice) tick() {
+
+	for range s.ticker.C {
+		log.Println("Tick!")
+	}
 }
